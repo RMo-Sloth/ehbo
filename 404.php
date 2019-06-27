997a2e43af09ffc794dfe3b9320215111e245e00
Rob Monhemius <robmonhemius@gmails.com>
2019-06-27 09:01:37 +0200

    404 page added

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+  get_header();
+?>
+
+<main>
+  <section>
+    <h1>404: Deze pagina bestaat niet</h1>
+    <p style='text-align: center'>De pagina die u probeerde te vinden bestaat niet ( meer ). Probeer de betreffende pagina te vinden via het navigatie-menu. Het is ook mogelijk dat de pagina geheel is verwijderd.</p>
+  </section>
+</main>
+
+<?php get_footer(); ?>
