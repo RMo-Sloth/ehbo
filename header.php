8aef2c74b25cb55d9f223184bdccb118d4b02443
Rob Monhemius <robmonhemius@gmails.com>
2019-11-13 19:31:16 +0100

    removed old title logic

--- header.php
+++ header.php
@@ -3,20 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" >
   <meta name="viewport" content="width=device-width, initial-scale=1" >
-  <?php
-    if( is_home() === true ) {
-      $title = '';
-    } else if( is_category() && have_posts() ) {
-      $categories = get_the_category();
-      $title = $categories[0]->name;
-    } else {
-      $title = get_the_title();
-    }
-    $title = $title ? $title.' - ' : '' ;
-
-    wp_head();
-  ?>
-  <title><?php echo $title; ?>EHBO Berkenwoude</title>
+  <?php wp_head(); ?>
 </head>
 
   <body <?php body_class(); ?> >
