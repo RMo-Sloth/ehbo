5adc1a9d7a9243f3aa8a4e3fa1e417f2cfa70509
Rob Monhemius <robmonhemius@gmails.com>
2019-11-13 21:42:37 +0100

    refactored featured-items

--- modules/featured-items.php
+++ modules/featured-items.php
@@ -2,14 +2,24 @@
 
 class FeaturedItems {
   private $stickyPostIndexes;
+  private $posts;
 
   function __construct() {
     $this->stickyPostIndexes = get_option( 'sticky_posts' );
   }
   function compile() {
+    $this->createPosts();
+
     $this->openList();
     $this->addPosts();
     $this->closeList();
+  }
+  private function createPosts() {
+    foreach ( $this->stickyPostIndexes as $stickyPostIndex ) {
+      $query = new WP_Query( 'p=' . $stickyPostIndex );
+      if( !is_null( $query->post ) ) // wp doesn't return private posts
+        $this->posts[] = $query->post;
+    }
   }
   private function openList() {
     echo '<ul class=\'featured-items\'>';
@@ -18,29 +28,24 @@ class FeaturedItems {
     echo '</ul>';
   }
   private function addPosts() {
-    while( count($this->stickyPostIndexes) > 0 ) {
-      $this->addPost();
-    }
+    $this->addPost();
   }
   private function addPost() {
-    $query = new WP_Query( 'p=' . array_pop($this->stickyPostIndexes) );
-    $post = $query->post;
-    if( is_null($query->post) ) { // wp doesn't return private posts
-      return;
+    foreach ( $this->posts as $post ) {
+      if( get_the_post_thumbnail_url( $post ) )
+        $postImageUrl =  get_the_post_thumbnail_url( $post );
+      else
+        $postImageUrl = get_template_directory_uri().'/images/default-background-1.jpg';
+
+      echo "<li class='card'><a href='".get_permalink( $post )."'>";
+
+      echo '<figure>';
+      echo '<img src="'.$postImageUrl.'" />';
+      echo '</figure>';
+
+      echo "<h2 class='title'>$post->post_title</h2>";
+
+      echo '</a></li>';
     }
-    if( get_the_post_thumbnail_url( $post ) )
-      $postImageUrl =  get_the_post_thumbnail_url( $post );
-    else
-      $postImageUrl = get_template_directory_uri().'/images/default-background-1.jpg';
-
-    echo "<li class='card'><a href='".get_permalink( $post )."'>";
-
-    echo '<figure>';
-    echo '<img src="'.$postImageUrl.'" />';
-    echo '</figure>';
-
-    echo "<h2 class='title'>$post->post_title</h2>";
-
-    echo '</a></li>';
   }
 }
