1bf0a8069b788806aef5d6112f74365d71ad565b
Rob Monhemius <robmonhemius@gmails.com>
2019-11-30 20:57:31 +0100

    alt attributes for thumbnail images

--- category.php
+++ category.php
@@ -13,18 +13,17 @@
   <?php if ( have_posts() ) {
     while ( have_posts() ) {
       the_post();
-
       if( has_post_thumbnail() ) {
         $postImageUrl =  get_the_post_thumbnail_url();
+        $alt = get_post_meta ( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
       } else {
         $postImageUrl =  get_template_directory_uri().'/images/default-background-1.jpg';
-      }
-      
-      ?>
+        $alt = '';
+      }?>
       <li class='card'>
       <a href="<?php echo get_permalink(); ?>">
         <figure>
-          <img src="<?php echo $postImageUrl; ?>">
+          <img src="<?php echo $postImageUrl; ?>" alt="<?php echo $alt; ?>" >
 
         </figure>
         <h2 class="title"><?php the_title(); ?></h2>
