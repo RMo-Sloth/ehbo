8b3cb70007431043252a3a0df3f4322353ca967b
Rob Monhemius <robmonhemius@gmail.com>
2023-02-06 04:06:21 +0100

    3d-logos-homepage

--- home.php
+++ home.php
@@ -17,7 +17,7 @@
 
   <section class='max-container-width'>
     <h1 class='section-header'>wij zijn aangesloten bij</h1>
-    <p>Onze vereniging is aangesloten bij een aantal landelijke organisaties: EHBO Nederland, het Oranje Kruis en de Hartstichting.</p>
+    <p>Onze vereniging is aangesloten bij een aantal landelijke organisaties: <a href='https://www.ehbonederland.nl/' target='_blank' rel="noopener" >EHBO Nederland</a>, <a href='https://www.hetoranjekruis.nl/' target='_blank' rel="noopener" >het Oranje Kruis</a> en de <a href='https://www.hartstichting.nl/' target='_blank' rel="noopener" >Hartstichting.</a></p>
     <div class='associations'>
       <a href='https://www.hartstichting.nl/' target='_blank' rel="noopener" >
         <img src='<?php echo get_template_directory_uri().'/images/de-hartstichting.png'; ?>' alt='logo van de hartstichting' />
